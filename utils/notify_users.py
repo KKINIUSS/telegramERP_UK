85a0676e15eb72933818c8979b0d12b4ef3e3ae5
KKINIUSS <kisell22@yanex.ru>
2021-09-23 20:27:11 +0300

    23.09.21

--- utils/notify_users.py
+++ utils/notify_users.py
@@ -1,9 +1,19 @@
 from aiogram import dispatcher
-from database.connect_db import conn, cur
 from aiogram import Dispatcher
 from keyboards.default.menu import menu
+import mariadb
+from data.config import user, password, host, port, database
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
 async def on_startup_notify_users(dp:Dispatcher):
+    conn = mariadb.connect(
+        user=user,
+        password=password,
+        host=host,
+        port=port,
+        database=database
+    )
+    cur = conn.cursor()
     text = "Бот был перезапущен, введите /start, чтобы продолжить."
     conn.commit()
     cur.execute("select name from tabTelegramUsers")
@@ -18,3 +28,4 @@ async def on_startup_notify_users(dp:Dispatcher):
             await dp.bot.send_message(i[0], text, reply_markup=bnt_inl)
         except:
             print("WARRING: user without telegramid")
+    conn.close()

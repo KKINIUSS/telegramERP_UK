307c82e7321795fddd2feef7e71be843274fdeb7
KKINIUSS <kisell22@yanex.ru>
2021-09-23 20:23:44 +0300

    23.09.21

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,6 +1,6 @@
 from .start import dp
 from .registration_handler import dp
 from .service_handler import dp
-from .login_handler import dp
+#from .login_handler import dp
 from .concierge_handler import  dp
 __all__ = ["dp"]

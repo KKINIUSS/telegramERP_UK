08851a57a7d8c3dfa5cb4efc83afd11a40e7322b
KKINIUSS <kisell22@yanex.ru>
2021-10-12 17:40:01 +0300

    12.10.21

--- handlers/users/concierge_handler.py
+++ handlers/users/concierge_handler.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from aiogram.types import Message
 from aiogram.utils.markdown import bold
-
+from keyboards.default.menu import menu
 from states.employee_state import employer
 from loader import dp
 from aiogram.dispatcher import FSMContext
@@ -22,21 +22,29 @@ async def show_guests(message: Message, state=FSMContext):
         database=database
     )
     cur = conn.cursor()
-    await message.answer("Таблица гостей: \n")
-    params = [datetime.now() - timedelta(hours=24), datetime.now()]
-    conn.commit()
-    sql = "select subject from tabTask where status='Активен' and creation between ? and ?"
-    cur.execute(sql, params)
-    str = cur.fetchall()
-    if(str):
-        a = []
-        for i in range(len(str)):
-            a.append("• " + str[i][0])
-        str = "\n".join(a)
+    cur.execute("select name, status, role from tabTelegramUsers where telegramid=?", [message.from_user.id])
+    k = cur.fetchall()
+    if (k):
+        await message.answer("Таблица гостей: \n")
+        params = [datetime.now() - timedelta(hours=24), datetime.now()]
+        conn.commit()
+        sql = "select subject from tabTask where status='Активен' and creation between ? and ?"
+        cur.execute(sql, params)
+        str = cur.fetchall()
+        if(str):
+            a = []
+            for i in range(len(str)):
+                a.append("• " + str[i][0])
+            str = "\n".join(a)
+        else:
+            str = "На данный момент гостей нет."
+        conn.close()
+        await message.answer(str)
     else:
-        str = "На данный момент гостей нет."
-    conn.close()
-    await message.answer(str)
+        await message.answer("Добрый день, нажмите кнопку 'Зарегистрироваться', чтобы пройти регистрацию.",
+                             reply_markup=menu)
+        conn.close()
+        await registration.start.set()
 @dp.message_handler(text="Найти гостя", state=employer.work)
 async def ans_find_guest(message: Message, state=FSMContext):
     await message.answer("Введите имя гостя.")
@@ -52,20 +60,27 @@ async def find_guest(message: Message, state=FSMContext):
         database=database
     )
     cur = conn.cursor()
-    params = "%" + message.text + "%"
-    cur.execute("select subject from tabTask where status='Активен' and subject like '%s'" % params)
-    str = cur.fetchall()
-    a = []
-    if str != []:
-        for i in range(len(str)):
-            a.append("• " + str[i][0])
-        mes = f"Список гостей по запросу '{message.text}'\n" + "\n".join(a)
-        await message.answer(mes)
+    cur.execute("select name, status, role from tabTelegramUsers where telegramid=?", [message.from_user.id])
+    k = cur.fetchall()
+    if(k):
+        params = "%" + message.text + "%"
+        cur.execute("select subject from tabTask where status='Активен' and subject like '%s'" % params)
+        str = cur.fetchall()
+        a = []
+        if str != []:
+            for i in range(len(str)):
+                a.append("• " + str[i][0])
+            mes = f"Список гостей по запросу '{message.text}'\n" + "\n".join(a)
+            await message.answer(mes)
+        else:
+            await message.answer("Гость не найден")
+        conn.close()
+        await employer.work.set()
     else:
-        await message.answer("Гость не найден")
-    conn.close()
-    await employer.work.set()
-
+        await message.answer("Добрый день, нажмите кнопку 'Зарегистрироваться', чтобы пройти регистрацию.",
+                             reply_markup=menu)
+        conn.close()
+        await registration.start.set()
 @dp.message_handler(text="Отправить сообщение охране", state=employer.work)
 async def call_security(message:Message, sate=FSMContext):
     conn = mariadb.connect(
@@ -76,19 +91,26 @@ async def call_security(message:Message, sate=FSMContext):
         database=database
     )
     cur = conn.cursor()
-    await send_message.security.set()
-    conn.commit()
-    cur.execute("select name, full_name, location, role from tabTelegramUsers where role='Охранник' and name != %s"%(message.from_user.id))
-    data = cur.fetchall()
-    if data != []:
-        await message.answer("Введите сообщение для охраны", reply_markup=back_menu)
-        conn.close()
+    cur.execute("select name, status, role from tabTelegramUsers where telegramid=?", [message.from_user.id])
+    k = cur.fetchall()
+    if (k):
         await send_message.security.set()
+        conn.commit()
+        cur.execute("select name, full_name, location, role from tabTelegramUsers where role='Охранник' and name != %s"%(message.from_user.id))
+        data = cur.fetchall()
+        if data != []:
+            await message.answer("Введите сообщение для охраны", reply_markup=back_menu)
+            conn.close()
+            await send_message.security.set()
+        else:
+            await message.answer("Нет охранников", reply_markup=menu_concierge)
+            conn.close()
+            await employer.work.set()
     else:
-        await message.answer("Нет охранников", reply_markup=menu_concierge)
+        await message.answer("Добрый день, нажмите кнопку 'Зарегистрироваться', чтобы пройти регистрацию.",
+                             reply_markup=menu)
         conn.close()
-        await employer.work.set()
-
+        await registration.start.set()
 
 @dp.message_handler(text="Назад", state= send_message.security)
 async def back_emp_menu(message: Message, state=FSMContext):
@@ -106,19 +128,27 @@ async def send_message_security(message: Message, state = FSMContext):
         database=database
     )
     cur = conn.cursor()
-    text = message.text
-    conn.commit()
-    cur.execute("select name from tabTelegramUsers where role='Охранник' and name != %s" % (
-        message.from_user.id))
-    data = cur.fetchall()
-    if data != []:
-        cur.execute("select full_name, role, location from tabTelegramUsers where name=%s" %(message.from_user.id))
-        mas = cur.fetchall()
-        from_employee = "Сообщение от: " + mas[0][0] + "\nДолжность: " + mas[0][1] + \
-                        "\nМестоположение: " + mas[0][2] + "\n\nСообщение: " + text
-        for i in data:
-            await bot.send_message(i[0], from_employee)
-    await message.answer(text="Сообщение отправлено", reply_markup=menu_concierge)
-    conn.close()
-    await employer.work.set()
+    cur.execute("select name, status, role from tabTelegramUsers where telegramid=?", [message.from_user.id])
+    k = cur.fetchall()
+    if (k):
+        text = message.text
+        conn.commit()
+        cur.execute("select name from tabTelegramUsers where role='Охранник' and name != %s" % (
+            message.from_user.id))
+        data = cur.fetchall()
+        if data != []:
+            cur.execute("select full_name, role, location from tabTelegramUsers where name=%s" %(message.from_user.id))
+            mas = cur.fetchall()
+            from_employee = "Сообщение от: " + mas[0][0] + "\nДолжность: " + mas[0][1] + \
+                            "\nМестоположение: " + mas[0][2] + "\n\nСообщение: " + text
+            for i in data:
+                await bot.send_message(i[0], from_employee)
+        await message.answer(text="Сообщение отправлено", reply_markup=menu_concierge)
+        conn.close()
+        await employer.work.set()
+    else:
+        await message.answer("Добрый день, нажмите кнопку 'Зарегистрироваться', чтобы пройти регистрацию.",
+                             reply_markup=menu)
+        conn.close()
+        await registration.start.set()
 
--- handlers/users/service_handler.py
+++ handlers/users/service_handler.py
@@ -14,8 +14,25 @@ from keyboards.default.back_from_service import bac_service_menu
 
 @dp.message_handler(text="Добавить гостя", state=user_status.logined)
 async def add_geust(message: Message, state: FSMContext):
-    await message.answer("Введите информацию о госте(ФИО, марку/номер машины)", reply_markup=bac_service_menu)
-    await add_guest.get_name.set()
+    conn = mariadb.connect(
+        user=user,
+        password=password,
+        host=host,
+        port=port,
+        database=database
+    )
+    cur = conn.cursor()
+    conn.commit()
+    cur.execute("select name, status, role from tabTelegramUsers where telegramid=?", [message.from_user.id])
+    k = cur.fetchall()
+    if (k):
+        await message.answer("Введите информацию о госте(ФИО, марку/номер машины)", reply_markup=bac_service_menu)
+        await add_guest.get_name.set()
+    else:
+        await message.answer("Добрый день, нажмите кнопку 'Зарегистрироваться', чтобы пройти регистрацию.",
+                             reply_markup=menu)
+        conn.close()
+        await registration.start.set()
 
 @dp.message_handler(state=add_guest.get_name)
 async def get_name(message: Message, state: FSMContext):
@@ -28,35 +45,42 @@ async def get_name(message: Message, state: FSMContext):
     )
     cur = conn.cursor()
     conn.commit()
-    str_message = message.text
-    if(str_message == 'Назад'):
-        await message.answer("Нажмите 'Добавить гостя', чтобы выпустить пропуск.", reply_markup=menu_customer)
-        conn.close()
-        await user_status.logined.set()
-    elif(len(str_message)>40):
-        await message.answer("Невеный формат\nИнфорация о госте должна содержать не более 40 символов, повторите снова")
-        await message.answer("Введите информацию о госте(ФИО, марку/номер машины)", reply_markup=bac_service_menu)
-        conn.close()
-        await add_guest.get_name.set()
+    cur.execute("select name, status, role from tabTelegramUsers where telegramid=?", [message.from_user.id])
+    k = cur.fetchall()
+    if (k):
+        str_message = message.text
+        if(str_message == 'Назад'):
+            await message.answer("Нажмите 'Добавить гостя', чтобы выпустить пропуск.", reply_markup=menu_customer)
+            conn.close()
+            await user_status.logined.set()
+        elif(len(str_message)>40):
+            await message.answer("Невеный формат\nИнфорация о госте должна содержать не более 40 символов, повторите снова")
+            await message.answer("Введите информацию о госте(ФИО, марку/номер машины)", reply_markup=bac_service_menu)
+            conn.close()
+            await add_guest.get_name.set()
+        else:
+            guest_name = message.text
+            await state.update_data(guest_name=guest_name)
+            cur.execute("select full_name from tabTelegramUsers where telegramid=?", [message.from_user.id])
+            customers = cur.fetchall()
+            customer = customers[0][0]
+            data = await state.get_data()
+            sql = "select count(name)+1 from tabTask"
+            cur.execute(sql)
+            answerDb = cur.fetchall()
+            sql = "INSERT INTO tabTask (name, subject, creation, owner, status, exp_start_date, exp_end_date, modified) VALUES (%s, %s, %s, %s, 'Активен', %s, %s, %s)"
+            count_task = "TASK-2021-" + str(answerDb[0][0])
+            today = datetime.datetime.now()
+            delta = datetime.timedelta(days=1)
+            zavtra = today + delta
+            mas = [count_task, data.get("guest_name"), today, customer, today, zavtra, today]
+            cur.execute(sql, mas)
+            conn.commit()
+            await message.answer("Гость успешно добавлен", reply_markup=menu_customer)
+            conn.close()
+            await user_status.logined.set()
     else:
-        guest_name = message.text
-        await state.update_data(guest_name=guest_name)
-        cur.execute("select full_name from tabTelegramUsers where telegramid=?", [message.from_user.id])
-        customers = cur.fetchall()
-        customer = customers[0][0]
-        data = await state.get_data()
-        sql = "select count(name)+1 from tabTask"
-        cur.execute(sql)
-        answerDb = cur.fetchall()
-        sql = "INSERT INTO tabTask (name, subject, creation, owner, status, exp_start_date, exp_end_date, modified) VALUES (%s, %s, %s, %s, 'Активен', %s, %s, %s)"
-        count_task = "TASK-2021-" + str(answerDb[0][0])
-        today = datetime.datetime.now()
-        delta = datetime.timedelta(days=1)
-        zavtra = today + delta
-        mas = [count_task, data.get("guest_name"), today, customer, today, zavtra, today]
-        cur.execute(sql, mas)
-        conn.commit()
-        await message.answer("Гость успешно добавлен", reply_markup=menu_customer)
+        await message.answer("Добрый день, нажмите кнопку 'Зарегистрироваться', чтобы пройти регистрацию.",
+                             reply_markup=menu)
         conn.close()
-        await user_status.logined.set()
-
+        await registration.start.set()
